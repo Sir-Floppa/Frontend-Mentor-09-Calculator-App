0a7b1381290c1f62a3d901138cd0581533906cf9
Sir-Floppa <nicolas.donoso.ge@outlook.com>
2023-05-17 00:42:59 -0400

    separated keyboard on its own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MonitorComponent } from './monitor/monitor.component';
+import { KeyboardComponent } from './keyboard/keyboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MonitorComponent
+    MonitorComponent,
+    KeyboardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/keyboard/keyboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-keyboard',
+  templateUrl: './keyboard.component.html',
+  styleUrls: ['./keyboard.component.scss']
+})
+export class KeyboardComponent {
+
+}
