0c686f9d1c9f18d498010be6e0155b4828a07bc9
Sir-Floppa <nicolas.donoso.ge@outlook.com>
2023-05-16 23:43:57 -0400

    added monitor and modularized header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MonitorComponent } from './monitor/monitor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MonitorComponent
   ],
   imports: [
     BrowserModule,
