9ea76b223b3ff964731ad893e1a2de00e0fdb107
Sir-Floppa <nicolas.donoso.ge@outlook.com>
2023-05-18 00:59:18 -0400

    added hover effects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = '09-Calculator-App';
+export class AppComponent implements OnInit {
+  private body: HTMLBodyElement = <HTMLBodyElement>(document.querySelector('body'));
+  
+  ngOnInit(): void {
+    
+  }
+  
 }
